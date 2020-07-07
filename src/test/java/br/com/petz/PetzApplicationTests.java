d7efcd832890bfdb05498dc7217f413d0786499e
diegolima <diego.ferreiradelima@gmail.com>
2020-07-07 16:06:14 -0300

    ajuste

--- src/test/java/br/com/petz/PetzApplicationTests.java
+++ src/test/java/br/com/petz/PetzApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.testejava.testeusuarioservices;
+package br.com.petz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TesteUsuarioServicesApplicationTests {
+class PetzApplicationTests {
 
 	@Test
 	void contextLoads() {
